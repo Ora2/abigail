da616e9965399824b025319149f6fb5dd6b17fb4
liyida <984689593@qq.com>
2021-06-08 13:45:22 +0800

    Initial commit

---
+++ media-manager/src/test/java/io/yd/mediamanager/MediaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yd.mediamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
